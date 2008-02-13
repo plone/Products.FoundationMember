4d24b308508e5ce4c409529a5677e52614b10807
Alex Clark <aclark@aclark.net>
2008-02-13 19:20:12 +0000

    Add in __init__.py from product (could not be svn moved because target exists).
    svn path=/Products.FoundationMember/trunk/; revision=19278

--- Products/FoundationMember/__init__.py
+++ Products/FoundationMember/__init__.py
@@ -1,3 +1,27 @@
 
+from Globals import package_home
+from Products.Archetypes.public import process_types, listTypes
+from Products.CMFCore import utils
+from Products.CMFCore.DirectoryView import registerDirectory
+import os, os.path
+
+from config import ADD_CONTENT_PERMISSION, PROJECTNAME, SKINS_DIR, GLOBALS
+
+registerDirectory(SKINS_DIR, GLOBALS)
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+    import types
+
+    content_types, constructors, ftis = process_types(
+        listTypes(PROJECTNAME),
+        PROJECTNAME)
+    
+    utils.ContentInit(
+        PROJECTNAME + ' Content',
+        content_types      = content_types,
+        permission         = ADD_CONTENT_PERMISSION,
+        extra_constructors = constructors,
+        fti                = ftis,
+        ).initialize(context)
