3ab900d40cf3696c031f1e9cb4a2576c68ff82d1
Alex Clark <aclark@aclark.net>
2008-02-13 19:32:19 +0000

    I'm not sure what the policy is here, but I'm assuming since the branch works it is ok to merge to trunk (provides Plone 3 compatibility.)
    svn path=/Products.FoundationMember/trunk/; revision=19281

--- Products/FoundationMember/Extensions/Install.py
+++ Products/FoundationMember/Extensions/Install.py
@@ -1,8 +1,13 @@
 from Products.Archetypes.atapi import listTypes
 from Products.Archetypes.Extensions.utils import installTypes, install_subskin
 from Products.FoundationMember.config import *
-from Products.FoundationMember.Extensions import Workflow
 from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility 
+from Products.GenericSetup.interfaces import ISetupTool
+
+EXTENSION_PROFILES = ('Products.FoundationMember:default',) 
+
+import transaction
 
 from StringIO import StringIO
 
@@ -15,19 +20,9 @@ def install(self):
 
     install_subskin(self, out, GLOBALS)
 
-    Workflow.install()
     wf_tool = getToolByName(self, 'portal_workflow')
-
-    if not 'foundation_member_workflow' in wf_tool.objectIds():
-        wf_tool.manage_addWorkflow('foundation_member_workflow (Approval process for foundation members)',
-                                   'foundation_member_workflow')
-    wf_tool.updateRoleMappings()
-
-    print >> out, 'Installed foundation_member_workflow.'
-
     wf_tool.setChainForPortalTypes(pt_names=['FoundationMember'] , chain='foundation_member_workflow')
     print >> out, 'Set foundation_member_workflow as default for foundation members'
-
 
     ft = getToolByName(self, 'portal_factory')
     portal_factory_types = ft.getFactoryTypes().keys()
@@ -36,6 +31,18 @@ def install(self):
             portal_factory_types.append(t)
     ft.manage_setPortalFactoryTypes(listOfTypeIds=portal_factory_types)
     print >> out, 'New types use portal_factory'
+
+    portal_setup = getUtility(ISetupTool)
+    quickinstaller = getToolByName(self, 'portal_quickinstaller')
+
+    # The following section is boilerplate code that can be reused when you 
+    # need to invoke a GenericSetup profile from Install.py. 
+    for extension_id in EXTENSION_PROFILES: 
+        portal_setup.setImportContext('profile-%s' % extension_id) 
+        portal_setup.runAllImportSteps(purge_old=False) 
+        product_name = extension_id.split(':')[0] 
+        quickinstaller.notifyInstalled(product_name) 
+        transaction.savepoint() 
 
     print >> out, "Successfully installed %s." % PROJECTNAME
     return out.getvalue()
