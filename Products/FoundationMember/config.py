8da8a912bc79b920b46ee3f4b65b99aaa045bfaa
Alex Clark <aclark@aclark.net>
2008-02-13 18:27:55 +0000

    Relocate product files to egg.
    svn path=/Products.FoundationMember/trunk/; revision=19269

---
+++ Products/FoundationMember/config.py
@@ -0,0 +1,9 @@
+from Products.CMFCore.permissions import AddPortalContent
+
+ADD_CONTENT_PERMISSION = AddPortalContent
+VIEW_PF_MEMBERS_DETAIL = "Foundation Member: View Detail"
+MANAGE_PF_MEMBERS = "Foundation Member: Manage Members"
+PROJECTNAME = "FoundationMember"
+SKINS_DIR = 'skins'
+
+GLOBALS = globals()
