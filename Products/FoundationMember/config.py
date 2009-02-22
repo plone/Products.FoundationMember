667001a7983ea26225009f07122a99516dd00eba
Matthew Wilkes <matthew@matthewwilkes.co.uk>
2009-02-22 18:54:46 +0000

    Added an Add Foundation Member permission to allow fine-grained control of where in a site foundation members can be added.
    svn path=/Products.FoundationMember/trunk/; revision=25237

--- Products/FoundationMember/config.py
+++ Products/FoundationMember/config.py
@@ -1,6 +1,4 @@
-from Products.CMFCore.permissions import AddPortalContent
-
-ADD_CONTENT_PERMISSION = AddPortalContent
+ADD_CONTENT_PERMISSION = "Foundation Member: Add Foundation Member"
 VIEW_PF_MEMBERS_DETAIL = "Foundation Member: View Detail"
 MANAGE_PF_MEMBERS = "Foundation Member: Manage Members"
 PROJECTNAME = "FoundationMember"
